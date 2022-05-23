5ca9e3241729e2e6856ba622cddd9dacf07b52e8
lthorel <loickthorel1999@gmail.com>
2022-05-23 21:53:18 +0200

    First Commit

---
+++ WebHookTimerDiscord/Counter.cs
@@ -0,0 +1,41 @@
+﻿namespace WebHookTimerDiscord;
+
+public static class Counter
+{
+    const string ColorBlue = "1F61E6";
+    const string ColorGreen = "80E61F";
+    const string ColorRed = "E71F1F";
+    const string ColorPurple = "C61FE6";
+    const string ColorYellow = "E6C71F";
+    const string ColorYellow2 = "FFDC03";
+    const string ColorGreen2 = "03D8A6";
+    const string ColorFuchsia = "FF00EF";
+    const string ColorBlack = "000000";
+    const string ColorRed2 = "C70039";
+    private static readonly List<object> Objects = new();
+
+    public static Task<List<object>> CountDownUntilBleachCameOut()
+    {
+        object objectAmz = new
+        {
+            title = "Sortie de Bleach",
+            description = "Il ne reste que "+ CounterUntilCameOut() + " jours avant la sortie de Bleach" ,
+            color = int.Parse(ColorRed2, System.Globalization.NumberStyles.HexNumber),
+        };
+
+        Objects.Add(objectAmz);
+        return Task.FromResult(Objects);
+    }
+
+    private static int CounterUntilCameOut()
+    {
+        var dateTime = DateTime.Today;
+        Console.WriteLine(dateTime);
+        var dateTimeUntil = new DateTime(2022, 10, 01);
+        Console.WriteLine(dateTimeUntil);
+
+        var getDaysUntil = (dateTimeUntil - dateTime).Days;
+        Console.WriteLine(getDaysUntil);
+        return getDaysUntil;
+    }
+}
---
+++ WebHookTimerDiscord/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Discord;
+using Newtonsoft.Json;
+
+namespace WebHookTimerDiscord;
+
+public static class Program
+{
+    private static HttpClient? Client { get; } = new();
+
+    private static async Task Main()
+    {
+        var webHookId = "";
+        var webHookToken = "";
+
+        var items = await Counter.CountDownUntilBleachCameOut();
+        
+        var successWebHook = new
+        {
+            embeds = items,
+        };
+        
+        if (successWebHook == null) throw new ArgumentNullException(nameof(successWebHook));
+
+        var endPoint = $"https://discordapp.com/api/webhooks/{webHookId}/{webHookToken}";
+
+        var content = new StringContent(JsonConvert.SerializeObject(successWebHook), Encoding.UTF8, "application/json");
+
+        Client?.PostAsync(endPoint, content).Wait();
+        
+    }
+}
